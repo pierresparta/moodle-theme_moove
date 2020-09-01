3c679646fd73a665c0934ddab6147efb2fbdaf7b
pierresparta <64263051+pierresparta@users.noreply.github.com>
2020-09-01 17:32:32 +0200

    Update icon_system.php

--- classes/util/icon_system.php
+++ classes/util/icon_system.php
@@ -285,6 +285,7 @@ class icon_system extends icon_system_fontawesome {
             'core:i/nosubcat' => 'fa-plus-square-o',
             'core:i/notifications' => 'fa-bell',
             'core:i/open' => 'fa-folder-open',
+            'core:i/otherevent' => 'fa-calendar',
             'core:i/outcomes' => 'fa-tasks',
             'core:i/payment' => 'fa-money',
             'core:i/permissionlock' => 'fa-lock',
